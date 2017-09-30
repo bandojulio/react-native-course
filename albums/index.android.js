d923ea61e0ef952a76f9858da5e20244b1dd3fd0
Julio Bando <julio.kenji.bando@accenture.com>
2017-09-30 14:50:12 -0300

    initial commit

---
+++ albums/index.android.js
@@ -0,0 +1,16 @@
+// Index.ios.js - iOS development goes here
+
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => {
+  return (
+    <Header />
+  );
+};
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
