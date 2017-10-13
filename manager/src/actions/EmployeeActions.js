2cef716025a4057d5a04462aa427fc3b030c11d7
Julio Bando <julio.kenji.bando@accenture.com>
2017-10-13 02:37:17 -0300

    working with firebase

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,10 +1,54 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+import { Actions } from 'react-native-router-flux';
 import {
-    EMPLOYEE_UPDATE
+    EMPLOYEE_UPDATE,
+    EMPLOYEE_CREATE,
+    EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
-export const employeeUpdate = ({ prop, value }) = {
-    return ({
+export const employeeUpdate = ({ prop, value }) => {
+    return {
         type: EMPLOYEE_UPDATE,
         payload: { prop, value }
-    });
+    };
+};
+
+export const employeeCreate = ({ name, phone, shift}) => {
+    const { currentUser } = firebase.auth();
+    
+    return (dispatch) => {
+        firebase.firestore().collection(`users/${currentUser.uid}/employees`)
+            .add({ name, phone, shift })
+            .then(() => {
+                dispatch({ type: EMPLOYEE_CREATE });
+                Actions.employeeList({ type: 'reset' });
+            });
+    };
+};
+
+export const employeesFetch = () => {
+    const { currentUser } = firebase.auth();
+    
+    return (dispatch) => {
+        // firebase.firestore().collection(`users/${currentUser.uid}/employees`)
+        //     .get()
+        //     .then( snapshot => {
+        //         let employees = {};
+        //         snapshot.forEach(employee => {
+        //             employees[employee.id] = employee.data();
+        //         });
+        //         dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: employees });
+        //     });
+        
+        firebase.firestore().collection(`users/${currentUser.uid}/employees`)
+            .onSnapshot( snapshot => {
+                let employees = {};
+                snapshot.forEach(employee => {
+                    employees[employee.id] = employee.data();
+                });
+                dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: employees });
+            });
+            
+    };
 };
